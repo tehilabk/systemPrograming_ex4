c09be6ec438214f5287d33d6778a587e0bde8fec
DorBaram <dorbaram@gmail.com>
2021-01-12 20:56:29 +0200

    added func3

--- frequency.c
+++ frequency.c
@@ -29,6 +29,22 @@ return new_node;
 }
 
 
+char* garbageRemover(char* c,int size){
+    char* string = (char*)malloc(sizeof(char)*(size+1));
+    if(string == NULL)
+        ;//TODO free tree
+    int i,j=0;
+    for (i = 0; i < size; i++)
+    {
+        if ((c[i]>='a' && c[i]<='z') || c[i]==' ' || c[i]=='\n' || c[i]=='\t' || c[i]=='\r' ||c[i]=='\0')
+        {
+            string[j]=c[i];
+            j++;
+        }
+    }
+    string=(char)realloc(string,sizeof(char)*(j+1));
+    return string;
+}
 
 char* toLower(char* c, int size){
 for (int i = 0; i < size; i++)
