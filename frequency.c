f7e285a49810133d268221b4ef693afe7de94688
Tehila <tehilabk123@gmail.com>
2021-01-12 19:36:29 +0200

    start the code

--- frequency.c
+++ frequency.c
@@ -2,3 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define NUM_LETTERS ((int)26)
+
+typedef enum {FALSE=0, TRUE=1} boolean;
+
+typedef struct node {
+char letter;
+long unsigned int count;
+struct node* children[NUM_LETTERS];
+} node;
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argcv[])
+{
+ 
+
+
+return 0;
+}
+
