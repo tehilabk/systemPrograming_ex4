f4f611794f37d2870d62371d6f36962d9e64a403
DorBaram <dorbaram@gmail.com>
2021-01-15 12:24:36 +0200

    plz work

--- frequency.c
+++ frequency.c
@@ -89,7 +89,7 @@ char* garbageRemover(char* c,int size){
     for (i = 0; i < size; i++)
     {
         if ((c[i]>='a' && c[i]<='z') || c[i]==' ' || c[i]=='\n' || c[i]=='\t' || c[i]=='\r' ||c[i]=='\0')
-        {//*word = (char*) realloc(word, sizeof(char)*(index+1));
+        {
             *string = (char*) realloc(string, sizeof(char)*(j+2));
             string[j]=c[i];
             j++;
